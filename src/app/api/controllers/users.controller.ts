29a4a6241a3cf192a9ac5ecaa88982f283e87bfd
Luis perotto <luis@team-xyz.com>
2023-09-20 14:41:12 +0700

    update cryptography

--- src/app/api/controllers/users.controller.ts
+++ src/app/api/controllers/users.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Query, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { UserService } from 'src/database/services/user.service';
+import { Decrypt, Encrypt } from 'src/helpers/encryption.helpers';
 
 @Controller('/api/v2/users')
 export class UsersController {
--- src/helpers/encryption.helpers.ts
+++ src/helpers/encryption.helpers.ts
@@ -7,6 +7,16 @@ import {
 } from 'crypto';
 import { readFileSync } from 'fs';
 
+interface DecryptInterface {
+  decrypted: string;
+  error: string | null;
+}
+
+interface EncryptInterface {
+  encrypted: string;
+  error: string | null;
+}
+
 const pass = readFileSync('src/config/secret.key').toString();
 
 const key = createHash('sha256')
@@ -14,23 +24,35 @@ const key = createHash('sha256')
   .digest('base64')
   .substr(0, 32);
 
-async function Encrypt(text) {
+async function Encrypt(text: string): Promise<EncryptInterface> {
   const iv = randomBytes(16);
   const cipher = createCipheriv('aes-256-ctr', key, iv);
   const result = Buffer.concat([iv, cipher.update(text), cipher.final()]);
-  return result;
+  const split = result.join('%*@%');
+  return {
+    encrypted: split,
+    error: null,
+  };
 }
 
-async function Decrypt(data) {
-  data = Array.isArray(JSON.parse(data))
-    ? Buffer.from(JSON.parse(data), 'binary')
-    : Buffer.from(data, 'binary');
+async function Decrypt(data: string | any): Promise<DecryptInterface> {
+  if (!data.includes('%*@%')) {
+    return {
+      decrypted: '',
+      error: 'encrypted data not valid',
+    };
+  }
+  data = data.split('%*@%');
+  data = Buffer.from(data, 'binary');
   const iv = data.slice(0, 16);
   data = data.slice(16);
 
   const dechiper = createDecipheriv('aes-256-ctr', key, iv);
   const result = Buffer.concat([dechiper.update(data), dechiper.final()]);
-  return result;
+  return {
+    decrypted: result.toString(),
+    error: null,
+  };
 }
 
 export { Encrypt, Decrypt };
